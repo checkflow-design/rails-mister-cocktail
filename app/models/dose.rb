b528a2beb8bad84fca4f60e184bd5a0e0fc78cec
Bonamy Yoann <yoann.bonamy79@gmail.com>
2020-08-13 14:47:31 +0200

    validates

--- app/models/dose.rb
+++ app/models/dose.rb
@@ -1,4 +1,9 @@
 class Dose < ApplicationRecord
   belongs_to :cocktail
   belongs_to :ingredient
+  has_many :cocktails
+
+  validates :description, presence: true
+  validates :cocktail, :ingredient, presence: true
+  validates :cocktail, uniqueness: { scope: :ingredient }
 end
