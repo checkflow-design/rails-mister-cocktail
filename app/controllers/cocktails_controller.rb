1f4ab035e7fbfb8030a7732bd1194339a6e38490
Bonamy Yoann <yoann.bonamy79@gmail.com>
2020-08-13 20:46:39 +0200

    cocktail

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,35 @@
 class CocktailsController < ApplicationController
+before_action :set_restaurant, only: [:show, :new]
+
   def index
     @cocktails = Cocktail.all
   end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.new
+  end
+
+  def create
+    @restaurant = Cocktail.new(cocktail_params)
+    if @cocktail.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render "new"
+    end
+
+  def
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_restaurant
+      @cocktails = Cocktail.find(params[:id])
+    end
+
+    # Only allow a trusted parameter "white list" through.
+    def restaurant_params
+      params.require(:cocktail).permit(:name)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :cocktails, only: :index
+  resources :cocktails, only: [:index, :show, :new] do
+    resources :doses
+  end
+
+  resources :ingredients
 end
