99891839134660f47f9b7c88680584a81d2cd3b9
Bonamy Yoann <yoann.bonamy79@gmail.com>
2020-08-14 11:59:03 +0200

    deploy

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -1,5 +1,5 @@
 class CocktailsController < ApplicationController
-before_action :set_restaurant, only: [:show, :new]
+  before_action :set_restaurant, only: [:show, :new]
 
   def index
     @cocktails = Cocktail.all
@@ -14,22 +14,23 @@ before_action :set_restaurant, only: [:show, :new]
 
   def create
     @restaurant = Cocktail.new(cocktail_params)
+
     if @cocktail.save
       redirect_to cocktail_path(@cocktail)
     else
       render "new"
     end
-
-  def
+  end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_restaurant
-      @cocktails = Cocktail.find(params[:id])
-    end
 
-    # Only allow a trusted parameter "white list" through.
-    def restaurant_params
-      params.require(:cocktail).permit(:name)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_restaurant
+    @cocktails = Cocktail.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def restaurant_params
+    params.require(:cocktail).permit(:name)
+  end
 end
